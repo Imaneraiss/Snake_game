7245a589b551ddbb5fe27d1c173961491a019499
Imane Raiss <imane.raiss100@gmail.com>
2025-12-06 17:04:36 +0100

    Game Over and Score

--- test.py
+++ test.py
@@ -37,14 +37,17 @@ window.geometry(f"{window_width}x{window_height}+{window_x}+{window_y}")
 #initialize game
 snake =Tile (5*TILE_SIZE,5*TILE_SIZE) #Snake's head
 food =Tile (10*TILE_SIZE,10*TILE_SIZE) #food's head
- 
+snake_body = [] # multiple snack tiles
 velocityX = 0 
 velocityY =0
-snake_body = [] # multiple snack tiles
+game_over = False
+score = 0
 
 def detected_diretion(e) : #e : event
    # print(e) #<KeyRelease event state=0x40000 keysym=Down keycode=40 x=487 y=408>
-    global velocityX , velocityY
+    global velocityX , velocityY ,game_over
+    if (game_over):
+        return
     if e.keysym =="Up" and velocityY != 1:
         velocityX = 0;
         velocityY = -1;
@@ -59,12 +62,23 @@ def detected_diretion(e) : #e : event
         velocityX = -1;
         velocityY = 0;
 def move():
-    global snake;
+    global snake, food, snake_body, game_over, score;
+    if(game_over):
+        return
+    
+    if(snake.x<0 or snake.x>= WINDOW_WIDTH or snake.y<0 or snake.y >=WINDOW_HEIGHT ):
+        game_over =True
+        return
+    for tile in snake_body :
+        if (snake.x == tile.x and snake.y == tile.y):
+            game_over= True
+            return
     # Collision
     if snake.x== food.x and snake.y == food.y :
         snake_body.append(Tile(food.x,food.y))
         food.x = random.randint(0,COLS-1)*TILE_SIZE
         food.y = random.randint(0,ROWS-1)*TILE_SIZE
+        score+=1
 
     #update snake body
     for i in range (len(snake_body)-1,-1,-1):
@@ -76,20 +90,26 @@ def move():
             prev_tile = snake_body[i-1]
             tile.x= prev_tile.x
             tile.y = prev_tile.y
-            
+
     snake.x += velocityX*TILE_SIZE    
     snake.y += velocityY*TILE_SIZE
 
 def draw () :
-    global snake
+    global snake , food , snake_body, game_over, score
     move()
     canvas.delete("all")
     #draw snake
-    canvas.create_rectangle(food.x,food.y,food.x+TILE_SIZE,food.y+TILE_SIZE,fill="red")
-    canvas.create_rectangle(snake.x,snake.y,snake.x+TILE_SIZE,snake.y+TILE_SIZE,fill="lime green")
+    canvas.create_rectangle(food.x,food.y,food.x+TILE_SIZE,food.y+TILE_SIZE,fill="yellow")
+    canvas.create_rectangle(snake.x,snake.y,snake.x+TILE_SIZE,snake.y+TILE_SIZE,fill="pink")
 
     for tiles in snake_body :
-        canvas.create_rectangle(tiles.x,tiles.y,tiles.x+TILE_SIZE,tiles.y+TILE_SIZE,fill="lime green")
+        canvas.create_rectangle(tiles.x,tiles.y,tiles.x+TILE_SIZE,tiles.y+TILE_SIZE,fill="pink")
+
+    if(game_over):
+        canvas.create_text(WINDOW_WIDTH/2 , WINDOW_HEIGHT/2, font ="Arial 20", text =f"Game Over: {score}", fill="white")
+    else:
+        canvas.create_text(30,20,font="Arial 10", text=f"Score: {score}", fill="white")
+
     window.after(200,draw) #without it, snake ll be draw one time and never move
 
 draw()
